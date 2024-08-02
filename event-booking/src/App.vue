2e595785d6942a44be991bb5b2fe3e19e480aae1
Marcus Hellams <mhellams@hotmail.com>
2024-08-02 02:52:40 -0400

    updates to event bookings

--- event-booking/src/App.vue
+++ event-booking/src/App.vue
@@ -3,7 +3,7 @@ import { BookingList, EventList } from '@/components';
 import { api } from '@/lib';
 import { type Booking, type Event } from '@/types';
 import { useQueries } from '@tanstack/vue-query';
-import { ref } from 'vue';
+import { ref, toRef } from 'vue';
 
 const addBookingPending = ref(false);
 
@@ -23,18 +23,22 @@ const queries = useQueries({
     },
   ],
 });
+
+const events = toRef(() => queries.value[0].data);
+const bookings = toRef(() => queries.value[1].data);
+const isFetching = toRef(() => queries.value[1].isFetching);
 </script>
 
 <template>
   <main class="container my-16">
     <h1>Event Booking App</h1>
     <h2>All Events</h2>
-    <EventList @get-is-pending="addBookingPending = $event" :events="queries[0]?.data" />
+    <EventList @get-is-pending="addBookingPending = $event" :events="events" />
     <h2>Your Bookings</h2>
     <BookingList
       :add-booking-pending="addBookingPending"
-      :is-fetching="queries[1].isFetching"
-      :bookings="queries[1]?.data"
+      :is-fetching="isFetching"
+      :bookings="bookings"
     />
   </main>
 </template>
